a0592a43f35c9bb303034c4f1881bddd4c1f43ad
rockyli123 <1026336191@qq.com>
2019-09-11 08:45:01 +0800

    Add files via upload

---
+++ test.rb
@@ -0,0 +1,63 @@
+
+node_name=ARGV[0]
+ip=ARGV[1]
+
+models_64 = Array.new
+models_64 << "vgg16"
+models_64 << "resnet50"
+models_64 << "resnet101"
+models_64 << "resnet152"
+models_64 << "inceptionv3"
+
+models_128 = Array.new
+models_128 << "vgg16"
+models_128 << "resnet50"
+models_128 << "resnet152"
+
+
+
+
+batch_size = 64
+gpu_nums = [1,4,8,16,32]
+
+models_64.each do |m|
+  gpu_nums.each do |n|
+    puts "===========#{m}====#{n}=========="
+    cmd = Array.new
+    if n < 16
+      for i in 0..n-1      
+        cmd << "HIP_VISIBLE_DEVICES=#{i} HSA_FORCE_FINE_GRAIN_PCIE=1 NCCL_DEBUG=INFO NCCL_DEBUG_SUBSYS=COLL NCCL_IB_DISABLE=0  python3.6 micro_benchmarking_pytorch.py --network #{m} --iterations 100 --dist-backend nccl --world-size #{n} --distributed_dataparallel --dist-url=\"tcp://#{ip}:54321\" --batch-size=#{batch_size} --rank #{i} >& ./log/#{n}_gpu_rank_#{i}_bs#{batch_size}_#{node_name}.log &"
+      end
+    else
+      for i in 0..7      
+        cmd << "HIP_VISIBLE_DEVICES=#{i} HSA_FORCE_FINE_GRAIN_PCIE=1 NCCL_DEBUG=INFO NCCL_DEBUG_SUBSYS=COLL NCCL_IB_DISABLE=0  python3.6 micro_benchmarking_pytorch.py --network #{m} --iterations 100 --dist-backend nccl --world-size #{n} --distributed_dataparallel --dist-url=\"tcp://#{ip}:54321\" --batch-size=#{batch_size} --rank #{i} >& ./log/#{n}_gpu_rank_#{i}_bs#{batch_size}_#{node_name}.log &"
+      end
+    end
+    puts cmd
+    puts "===================================="
+  end
+end
+
+
+batch_size = 128
+
+
+models_128.each do |m|
+  gpu_nums.each do |n|
+    puts "===========#{m}====#{n}=========="
+    cmd = Array.new
+    if n < 16
+      for i in 0..n-1      
+        cmd << "HIP_VISIBLE_DEVICES=#{i} HSA_FORCE_FINE_GRAIN_PCIE=1 NCCL_DEBUG=INFO NCCL_DEBUG_SUBSYS=COLL NCCL_IB_DISABLE=0  python3.6 micro_benchmarking_pytorch.py --network #{m} --iterations 100 --dist-backend nccl --world-size #{n} --distributed_dataparallel --dist-url=\"tcp://#{ip}:54321\" --batch-size=#{batch_size} --rank #{i} >& ./log/#{n}_gpu_rank_#{i}_bs#{batch_size}_#{node_name}.log &"
+      end
+    else
+      for i in 0..7      
+        cmd << "HIP_VISIBLE_DEVICES=#{i} HSA_FORCE_FINE_GRAIN_PCIE=1 NCCL_DEBUG=INFO NCCL_DEBUG_SUBSYS=COLL NCCL_IB_DISABLE=0  python3.6 micro_benchmarking_pytorch.py --network #{m} --iterations 100 --dist-backend nccl --world-size #{n} --distributed_dataparallel --dist-url=\"tcp://#{ip}:54321\" --batch-size=#{batch_size} --rank #{i} >& ./log/#{n}_gpu_rank_#{i}_bs#{batch_size}_#{node_name}.log &"
+      end
+    end
+    puts cmd
+    puts "===================================="
+  end
+end
+
+
